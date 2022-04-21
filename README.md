831fa4f8c520e78de5d902d0a5e4b25318f9eccd
Rcgil30 <rcgil@uninorte.edu.co>
2022-04-21 12:12:22 -0500

    Final code up to April 21st

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Esteban Perez
 
 ## UML Diagram
 ![UML](https://github.com/Rcgil30/Play_It/blob/main/bin/ClassDiagram.png)
+
+## Functional Requirement
+
+The main use of a functional requirement right now is the use of the graphic interface, since we do not have our database complete, the management of data is not defined yet, so the main functional part of the program, which is classifying the information to give a result of games to the user is not developed yet.
+
+As for the functionality of the graphic interface, we have a main menu with a button, which has the function of leading you to an input menu, were you can find a bunch of checkboxes for the genre and platforms, as well as the button to advance to the results, which will gather the information the user selected and assign it to the variables later to be used by the classes defined in the skeleton.
+
+Note: We're still lacking the implementation of the widgets for the inputs of the developer and the difficulty.
+
+Also, if you run the graphic inteface and it shows an error or doesn't show the main image, it might be because the path to the image file throws an error. In that case, please notify that through the GitHub issues.
--- graphicInterface.py
+++ graphicInterface.py
@@ -1,46 +1,59 @@
 import tkinter as tk
 from tkinter import ttk
 
-#Lists later to be used for the user selections
+# Lists later to be used for the user selections
 GenreList = []
 PlatformsList = []
+SwitchesList = []
 
-#Function to show the main menu to the user
+# Function to show the main menu to the user
+
+
 def MainMenu():
-    #tkinter's label is used in this case to include the background image, which is defined in the main algorithm
+    # Reset the window by destroying previous widgets
+    for ele in app.winfo_children():
+        ele.destroy()
+    # tkinter's label is used in this case to include the background image, which is defined in the main algorithm
     bglabel = tk.Label(app, image=bg)
     bglabel.pack()
-    #This button makes you switch to the next menu
+    # This button makes you switch to the next menu
     mainbutton = tk.Button(app, text="Start", width=20,
                            height=3, command=Menu2, bg="#F55A51", font=100)
-    mainbutton.place(x=510, y=565)
+    mainbutton.place(x=540, y=565)
 
 
 def Menu2():
-    #For loop to clear all the widgets from the root, so that we can start with the next menu
+    # For loop to clear all the widgets from the root, so that we can start with the next menu
     for ele in app.winfo_children():
         ele.destroy()
-    #In this canva we will show all the elements for the next menu
+    # Making a button to go back to the main menu
+    # In this canva we will show all the elements for the next menu
     canva = tk.Canvas(app)
     canva.pack()
-    #Label to show the background for the second menu
+    # Label to show the background for the second menu
     bg2label = tk.Label(canva, image=bg2)
     bg2label.pack()
-    #Switch that determines if a checkbutton is selected
+    backButton = tk.Button(canva, image=backImage, height=40, width=40, bd=1,
+                           relief="flat", overrelief="raised", bg="#FFCBB4", command=MainMenu)
+    backButton.place(x=10, y=670)
+    # Switch that determines if a checkbutton is selected
     swAction = tk.BooleanVar()
-    #We create a button with every Genre to allow the user to click a bunch of them, so that they
-    #can select multiple genres, generating then a list of genres, this will happen in the button to 
-    #show the results
+    SwitchesList.append(swAction)
+    # We create a button with every Genre to allow the user to click a bunch of them, so that they
+    # can select multiple genres, generating then a list of genres, this will happen in the button to
+    # show the results
     ActionButton = ttk.Checkbutton(canva, text="Action", variable=swAction)
     ActionButton.place(x=190, y=250)
     swAdventure = tk.BooleanVar()
-    #We build each button with the text of the Genre name and de variable that determines
-    #If it is selected or not, which we'll use then to get the elements of the list
+    SwitchesList.append(swAdventure)
+    # We build each button with the text of the Genre name and de variable that determines
+    # If it is selected or not, which we'll use then to get the elements of the list
     AdventureButton = ttk.Checkbutton(
         canva, text="Adventure", variable=swAdventure)
-    #With the place method, we get the buttons in a specific pixel of the window
+    # With the place method, we get the buttons in a specific pixel of the window
     AdventureButton.place(x=190, y=290)
     swCoop = tk.BooleanVar()
+    SwitchesList.append(swCoop)
     CoopButton = ttk.Checkbutton(canva, text="Coop", variable=swCoop)
     CoopButton.place(x=190, y=330)
     swFighting = tk.BooleanVar()
@@ -68,15 +81,62 @@ def Menu2():
         canva, text="Survival", variable=swSurvival)
     SurvivalButton.place(x=190, y=570)
 
-#Defining the root or master of our app
+    # After the Genre checkboxes, we define the Platform checkboxes
+    # We use the same methodology as in the Genre ones
+    swPc = tk.BooleanVar()
+    PcButton = ttk.Checkbutton(
+        canva, text="Pc", variable=swPc)
+    PcButton.place(x=410, y=250)
+    swPst = tk.BooleanVar()
+    PstButton = ttk.Checkbutton(
+        canva, text="PlayStation", variable=swPst)
+    PstButton.place(x=410, y=290)
+    swXbox = tk.BooleanVar()
+    XboxButton = ttk.Checkbutton(
+        canva, text="Xbox", variable=swXbox)
+    XboxButton.place(x=410, y=330)
+    swSwitch = tk.BooleanVar()
+    SwitchButton = ttk.Checkbutton(
+        canva, text="Nintendo Switch", variable=swSwitch)
+    SwitchButton.place(x=410, y=370)
+
+    # Now we create the scale for the difficulty
+    difficulty = tk.DoubleVar()
+    diffScale = tk.Scale(canva, variable=difficulty,
+                         from_=0, to=5, orient=tk.HORIZONTAL, bg="#44089B", bd=1,
+                         relief="flat")
+    diffScale.place(x=820, y=210)
+
+    # Finally, we define the button to advance to gather the info and show the final results
+    ResultsButton = tk.Button(canva, text="Get Recommendations",
+                              bg="#F55A51", height=3, width=20, command=getInfo, font=100)
+    ResultsButton.place(x=540, y=565)
+
+
+def getInfo():
+    """
+    In this part of the code, we will implement the selections class from the skeleton to gather the
+    information and store it in an object from that class, so that it can be used them for the classifier.
+    After getting this information, it will lead the user to the 3rd menu, which will show the final
+    results that we get from the process mentioned earlier.
+    """
+    menu3()
+
+
+def menu3():
+    pass
+
+
+# Defining the root or master of our app
 app = tk.Tk()
-#Changing the size of the app to 1280 x 720 so that it fits in every pc, no matter the resolution
-app.geometry("1280x720")
-#We set the window to not be resizable so that the image is not distorted
+# Changing the size of the app to 1280 x 720 so that it fits in every pc, no matter the resolution
+app.geometry("1280x720+120+65")
+# We set the window to not be resizable so that the image is not distorted
 app.resizable(False, False)
 app.title("Play It")
 bg = tk.PhotoImage(file="bin\Play it.png")
 bg2 = tk.PhotoImage(file="bin\Inputs menu.png")
+backImage = tk.PhotoImage(file="bin\Boton regresar.png")
 MainMenu()
 
 app.mainloop()
