1191b12b975f88d947cfc1350d7a390d3525a0c4
Rcgil30 <98895010+Rcgil30@users.noreply.github.com>
2022-04-20 22:58:23 -0500

    Update graphicInterface.py

--- graphicInterface.py
+++ graphicInterface.py
@@ -3,29 +3,40 @@ from tkinter import ttk
 
 GenreList = []
 
-
+#Function to show the main menu to the user
 def MainMenu():
+    #tkinter's label is used in this case to include the background image, which is defined in the main algorithm
     bglabel = tk.Label(app, image=bg)
     bglabel.pack()
+    #This button makes you switch to the next menu
     mainbutton = tk.Button(app, text="Start", width=20,
                            height=3, command=Menu2, bg="#F55A51", font=100)
     mainbutton.place(x=510, y=565)
 
 
 def Menu2():
+    #For loop to clear all the widgets from the root, so that we can start with the next menu
     for ele in app.winfo_children():
         ele.destroy()
+    #In this canva we will show all the elements for the next menu
     canva = tk.Canvas(app)
     canva.pack()
+    #Label to show the background for the second menu
     bg2label = tk.Label(canva, image=bg2)
     bg2label.pack()
-    # Switch que nos determina si el género está seleccionado
+    #Switch that determines if a checkbutton is selected
     swAction = tk.BooleanVar()
+    #We create a button with every Genre to allow the user to click a bunch of them, so that they
+    #can select multiple genres, generating then a list of genres, this will happen in the button to 
+    #show the results
     ActionButton = ttk.Checkbutton(canva, text="Action", variable=swAction)
     ActionButton.place(x=190, y=250)
     swAdventure = tk.BooleanVar()
+    #We build each button with the text of the Genre name and de variable that determines
+    #If it is selected or not, which we'll use then to get the elements of the list
     AdventureButton = ttk.Checkbutton(
         canva, text="Adventure", variable=swAdventure)
+    #With the place method, we get the buttons in a specific pixel of the window
     AdventureButton.place(x=190, y=290)
     swCoop = tk.BooleanVar()
     CoopButton = ttk.Checkbutton(canva, text="Coop", variable=swCoop)
@@ -55,11 +66,13 @@ def Menu2():
         canva, text="Survival", variable=swSurvival)
     SurvivalButton.place(x=190, y=570)
 
-
+#Defining the root or master of our app
 app = tk.Tk()
+#Changing the size of the app to 1280 x 720 so that it fits in every pc, no matter the resolution
 app.geometry("1280x720")
+#We set the window to not be resizable so that the image is not distorted
 app.resizable(False, False)
-app.title("Game Advocator")
+app.title("Play It")
 bg = tk.PhotoImage(file="bin\Play it.png")
 bg2 = tk.PhotoImage(file="bin\Inputs menu.png")
 MainMenu()
